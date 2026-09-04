b84baca797a2eaadd1ac61ddf8e59c5d1ea47cc6
Haikal <haikalisratulfajri@gmail.com>
2026-09-04 08:16:04 +0700

    feat: add restore and force delete mutations for Jenis and update GraphQL schema

---
+++ app/GraphQL/Mutations/jenis/jenisMutations.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\GraphQL\Mutations\jenis;
+
+use App\Models\ModelJenisPesan;
+
+class JenisMutations
+{
+
+    public function restore($_, array $args)
+    {
+        $jenis = ModelJenisPesan::withTrashed()->find($args['id']);
+        if (!$jenis) {
+            $jenis->restore();
+            return $jenis;
+        }
+    }
+}

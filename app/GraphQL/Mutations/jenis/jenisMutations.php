2b7816639c3a22e89305ecf74f8efae2bb2a07ad
Haikal <haikalisratulfajri@gmail.com>
2026-09-04 08:57:23 +0700

    feat: update GraphQL schemas for Jenis, Bagian, Level, Status, and UserProfile; implement restore and force delete mutations

--- app/GraphQL/Mutations/jenis/jenisMutations.php
+++ app/GraphQL/Mutations/jenis/jenisMutations.php
@@ -10,8 +10,17 @@ class JenisMutations
     public function restore($_, array $args)
     {
         $jenis = ModelJenisPesan::withTrashed()->find($args['id']);
-        if (!$jenis) {
+        if ($jenis) {
             $jenis->restore();
+            return $jenis;
+        }
+    }
+
+    public function forceDelete($_, array $args)
+    {
+        $jenis = ModelJenisPesan::withTrashed()->find($args['id']);
+        if ($jenis) {
+            $jenis->forceDelete();
             return $jenis;
         }
     }
